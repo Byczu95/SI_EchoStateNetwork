67c6a0c81631ce902cdfdb3b99c772c396b6ce05
Adam Matuszak <adam.j.matuszak@student.put.poznan.pl>
2017-05-07 22:37:20 +0200

    Implementing basic NN structure

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SI_ESN
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Network ESN = new Network(100,0.23234);
+            Console.ReadLine();
+        }
+    }
+
+}
